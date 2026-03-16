31d4d12636c14aa737211fada7a2dbd66086e196
sunjian <sunjian@100doc.com.cn>
2026-03-16 16:14:59 +0800

    加redis

--- window-order-backend/window-order-order/src/main/java/com/window/system/OrderApplication.java
+++ window-order-backend/window-order-order/src/main/java/com/window/system/OrderApplication.java
@@ -3,10 +3,12 @@ package com.window.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableCaching
 @SpringBootApplication
 @EnableAsync
 @MapperScan("com.window.system.mapper")
